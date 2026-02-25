9b2208b0654d30e799bb233c7babce8b5e66f4ba
MitzIgnacio <150250954+MitzIgnacio@users.noreply.github.com>
2026-02-25 19:31:53 +0800

    Search filter functionality + hotfixes

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,10 @@ Route::get('/user', function (Request $request) {
 
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->apiResource('articles', ArticleController::class);
 Route::middleware('auth:sanctum')->apiResource('categories', CategoryController::class);
 Route::get('/public/news', [PublicNewsController::class, 'index']);
+Route::get('/public/news/{article}', [PublicNewsController::class, 'show']);
+Route::middleware('auth:sanctum')->patch('/articles/{article}/publish', [ArticleController::class, 'publish']);
