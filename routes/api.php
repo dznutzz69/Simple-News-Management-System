ba06baae3d1e39fb0dd57fb7ba1c1042d307ccb3
kenkennnnnnnn <148937080+kenkennnnnnnn@users.noreply.github.com>
2026-02-25 20:41:37 +0800

    Implement feature dev11: article statistics and related APIs
    git rebase --continue#

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PublicNewsController;
 use App\Http\Controllers\CategoryController;
 
+
+use App\Http\Controllers\StatisticController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -20,3 +23,5 @@ Route::middleware('auth:sanctum')->apiResource('categories', CategoryController:
 Route::get('/public/news', [PublicNewsController::class, 'index']);
 Route::get('/public/news/{article}', [PublicNewsController::class, 'show']);
 Route::middleware('auth:sanctum')->patch('/articles/{article}/publish', [ArticleController::class, 'publish']);
+
+Route::get('/statistics', [StatisticController::class, 'index']);
