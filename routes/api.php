00de5bf6619cbacd6bd2a99318818f0d9bee592a
liyannedml <.liyannedml.com>
2026-02-25 17:05:48 +0800

    dev8

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PublicNewsController;
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +15,4 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->apiResource('articles', ArticleController::class);
 Route::middleware('auth:sanctum')->apiResource('categories', CategoryController::class);'categories', CategoryController::class);
+Route::get('/public/news', [PublicNewsController::class, 'index']);
